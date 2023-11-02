62705530c7f26ad275c7de1e4035f908ca0031df
Oscar Buitrago <o.buitragov@uniandes.edu.co>
2023-11-01 21:23:48 -0500

    Estructura inicial de proyecto

---
+++ app/src/main/java/com/team3/vinilos/ui/screens/StartScreen.kt
@@ -0,0 +1,9 @@
+package com.team3.vinilos.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun StartScreen() {
+    Text(text = "Esta es la pantalla de inicio")
+}
