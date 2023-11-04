df89e63362d6740bbab849599f9c03adbb248b58
jenniffergon <j.gonzalezg2@uniandes.edu.co>
2023-11-03 23:29:51 -0500

    pruebas locales service HU01 listado de albums - network

---
+++ app/src/test/java/com/team3/vinilos/AlbumsViewModelTest.kt
@@ -0,0 +1,30 @@
+package com.team3.vinilos
+
+import com.team3.vinilos.fake.FakeDataSource
+import com.team3.vinilos.fake.FakeNetworkAlbumsRepository
+import com.team3.vinilos.rules.TestDispatcherRule
+import com.team3.vinilos.ui.viewmodels.AlbumsUiState
+import com.team3.vinilos.ui.viewmodels.AlbumsViewModel
+import com.team3.vinilos.ui.viewmodels.ArtistsUiState
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+
+class AlbumsViewModelTest {
+    @get:Rule
+    val testDispatcher = TestDispatcherRule()
+
+    @Test
+    fun albumsViewModel_getAlbums_verifyAlbumsUiStateSuccess(){
+        runTest{
+            val albumsViewModel = AlbumsViewModel(
+                albumsRepository = FakeNetworkAlbumsRepository()
+            )
+            Assert.assertEquals(
+                AlbumsUiState.Success(FakeDataSource.listAlbum),
+                albumsViewModel.albumsUiState
+            )
+        }
+    }
+}
