2923918aa70f01ed21f16fa235b3d0dd74a15612
wcuadrosuniandes <w.cuadrosr@uniandes.edu.co>
2023-11-03 22:20:10 -0500

    Feature walter alejandra hu03 consultar listado artistas pruebas locales (#93)
    * FakerDataSource
    
    * FakerDataSource
    
    * Pruebas Locales Repository Listado de Artistas
    
    * Agregar pruebas repository
    
    * Agregar Test ViewModel Artistas
    
    ---------
    
    Co-authored-by: Alejandra Uniandes <ma.ninog12@uniandes.edu.co>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,11 +44,15 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.7"
     }
+
+
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
 }
 
 dependencies {
@@ -77,10 +81,13 @@ dependencies {
     implementation("io.github.serpro69:kotlin-faker:1.15.0")
 
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.05.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
 
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
